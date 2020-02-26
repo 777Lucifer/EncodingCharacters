20cd5c218e4320a07261df45a12c7ed8b6c303b3
777Lucifer <60865673+777Lucifer@users.noreply.github.com>
2020-02-26 06:40:15 -0500

    Add files via upload

---
+++ DspEnc.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class DspEnc
+{
+  static final int l=26;
+  static String encription(String str,int n,int t)
+  {
+    t = t % l - 1;
+    char arr[] =str.toCharArray();
+    int maxchar[] =new int[l];
+    for (int i=0;i<n ;i++ )
+      maxchar[arr[i] - 'a']++;
+      for (int i =0;i<n ;i++ )
+      {
+        if (maxchar[arr[i] - 'a'] % 2!=0)
+            {
+                int z=(arr[i] - 'a' + t) % l;
+                arr[i]= (char)(z + 'a');
+            }
+             else
+            {
+             int z =(arr[i] - 'a' + t);
+            if (z < 0)
+            z = 2+l;
+            arr[i] = (char)(z + 'a');
+            }
+     }
+
+
+
+  return String.valueOf(arr);
+}
+
+
+
+
+   public static void main(String[] args) {
+     System.out.println("enter charactors===");
+         Scanner input = new Scanner(System.in);
+         String s = input.nextLine();
+         int n = s.length();
+         int t = 3;
+         System.out.println(encription(s, n, t));
+
+  }
+}
